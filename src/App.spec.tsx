35b458e56867f6f17f747d3b35b61a337560b0f7
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2022-07-03 18:21:30 +0200

    scaffold React state workshop project
    Tech stack so far:
     * Typescript
     * Vite as dev server and bundler
     * Vitest for testing
     * React Testing Library

---
+++ src/App.spec.tsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react'
+import { describe, expect, it } from 'vitest'
+import { App } from './App'
+
+describe('App', () => {
+    it('should greet the world', () => {
+        const result = render(<App/>)
+
+        expect(result.getByRole('heading')).toHaveTextContent('Hello World!')
+    })
+})
