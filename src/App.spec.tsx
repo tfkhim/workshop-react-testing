cbec83d3e5d00f590c84493ff89fa2bd6a7edec6
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2022-07-30 23:51:16 +0200

    format files with Prettier

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -3,9 +3,9 @@ import { describe, expect, it } from 'vitest'
 import { App } from './App'
 
 describe('App', () => {
-    it('should greet the world', () => {
-        const result = render(<App/>)
+  it('should greet the world', () => {
+    const result = render(<App />)
 
-        expect(result.getByRole('heading')).toHaveTextContent('Hello World!')
-    })
+    expect(result.getByRole('heading')).toHaveTextContent('Hello World!')
+  })
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { FC } from "react";
+import { FC } from 'react'
 
 export const App: FC = () => {
-    return <h1>Hello World!</h1>
+  return <h1>Hello World!</h1>
 }
