ca0d0148cf2806422ef93b87a921744c8ba6e9f4
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2023-06-04 11:23:29 +0200

    Restructure the demo application
    This commit moves all demo application code into its own folder within
    the src folder.

---
+++ src/app/TodoListApp.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+
+export const TodoListApp: FC = () => {
+  return (
+    <>
+      <h1>Your tasks</h1>
+      <ul>
+        <li>Prepare an app for the exercises</li>
+      </ul>
+    </>
+  )
+}
