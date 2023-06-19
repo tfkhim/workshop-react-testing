ccb8a49de7adce0f4eea00d3abf69c0072281d9e
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2023-06-19 21:58:37 +0200

    Remove the old exercises

--- src/exercises/01-testing-components/ShowMoreButton.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, PropsWithChildren, useState } from 'react'
-
-export const ShowMoreButton: FC<PropsWithChildren> = ({ children }) => {
-  const [hidden, setHidden] = useState(true)
-
-  const show = () => setHidden(false)
-
-  return hidden ? <button onClick={show}>Show</button> : <>{children}</>
-}
